70cc001b0609e1697ca8639218b5eaa013edd97e
Pavi-weblogix <pavi@marketplacenetwork.com.au>
2026-04-07 16:01:25 +0530

    params count chamges

--- src/app/api/params-count/route.ts
+++ src/app/api/params-count/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   
-  const url = `https://admin.caravansforsale.com.au/wp-json/cfs/v1/params_count?${searchParams.toString()}`;
+  const url = `https://admin.motorhomesforsale.com.au/wp-json/cfs/v1/params_count?${searchParams.toString()}`;
   
   const response = await fetch(url, {
     headers: {
